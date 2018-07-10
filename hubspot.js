570152ee5a14e87044804812736960b7e8a78b6a
Adam <me@adamburns.org>
2018-07-10 17:13:31 -0500

    chrome.storage.sync.* non functional. To do: fix

--- hubspot.js
+++ hubspot.js
@@ -1,16 +1,27 @@
 var Name = document.getElementsByClassName("private-expanding-text-input__input")[0].value;
-localStorage.setItem('FR Name', Name);
+StorageArea.set({'FR_Name': Name}, function() {
+//todo
+console.log('FR Name is: ' + Name);
+});
+
 
 var Sponsor = document.getElementsByClassName("m-bottom-1 is--heading-7")[0].innerHTML;
-localStorage.setItem('FR Sponsor', Sponsor);
-
+StorageArea.set({'FR Sponsor': Sponsor}, function() {
+//todo
+});
 var ProjectType = document.getElementsByClassName("uiButton private-button private-button--default private-button--form private-form__control private-form__control--inline isInline private-button__dropdown-opener uiDropdown__button private-button--non-link")[2].getElementsByClassName("uiDropdown__buttonContents private-dropdown__button__contents")[0].innerText;
-localStorage.setItem('FR ProjectType', ProjectType);
+StorageArea.set({'FR ProjectType': ProjectType}, function() {
+//todo
+});
 
 var Executed = document.getElementById("uid-ctrl-17").value;
-localStorage.setItem('FR Executed', Executed);
+StorageArea.set({'FR Executed': Executed}, function() {
+//todo
+});
 
 var URL = window.location.href;
-localStorage.setItem('FR URL', URL);
+StorageArea.set({'FR URL': URL}, function() {
+//todo
+});
 
 alert("The HubSpot script has run, click me again on a SharePoint page to copy the data");
