0714b9aa41bf03de01f86184315fcb6df6059497
Adam <me@adamburns.org>
2018-07-10 11:04:55 -0500

    Updated hubspot.js to correctly store variables to localStorage

--- hubspot.js
+++ hubspot.js
@@ -1,16 +1,16 @@
 var Name = document.getElementsByClassName("private-expanding-text-input__input")[0].value;
-localStorage.setItem('FR Name', JSON.stringify(Name));
+localStorage.setItem('FR Name', Name);
 
 var Sponsor = document.getElementsByClassName("m-bottom-1 is--heading-7")[0].innerHTML;
-localStorage.setItem('FR Sponsor', JSON.stringify(Sponsor));
+localStorage.setItem('FR Sponsor', Sponsor);
 
 var ProjectType = document.getElementsByClassName("uiButton private-button private-button--default private-button--form private-form__control private-form__control--inline isInline private-button__dropdown-opener uiDropdown__button private-button--non-link")[2].getElementsByClassName("uiDropdown__buttonContents private-dropdown__button__contents")[0].innerText;
-localStorage.setItem('FR ProjectType', JSON.stringify(ProjectType));
+localStorage.setItem('FR ProjectType', ProjectType);
 
 var Executed = document.getElementById("uid-ctrl-17").value;
-localStorage.setItem('FR Executed', JSON.stringify(Executed));
+localStorage.setItem('FR Executed', Executed);
 
 var URL = window.location.href;
-localStorage.setItem('FR URL', JSON.stringify(URL));
+localStorage.setItem('FR URL', URL);
 
 alert("The HubSpot script has run, click me again on a SharePoint page to copy the data");
