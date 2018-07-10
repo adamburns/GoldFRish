675b52b190d17d712de4dbcd9df04e76840b1fd7
Adam <me@adamburns.org>
2018-07-09 22:34:34 -0500

    background.js now checks for current URL, and performs the associated js file

--- alert.js
+++ alert.js
@@ -1,2 +1,2 @@
 // alert user of invalid URL
-alert("Hello from your Chrome extension!")
+alert("Sorry, but you are not currently on a supported web page!\nPlease go to either Sharepoint.com or Hubspot.")
--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "alert.js"});
+   
+   if (location.href.includes(https://factrightllc367.sharepoint.com/_layouts/15/NewDocSet.aspx)) {
+      // if statement
+      chrome.t
+      chrome.tabs.executeScript(null, {file: "sharepoint.js"});
+   } else if (location.href.includes(https://app.hubspot.com/contacts/2777169/deal/299043516/properties)) {
+      // else if statement
+      chrome.t
+      chrome.tabs.executeScript(null, {file: "hubspot.js"});
+   } else {
+      chrome.t
+      chrome.tabs.executeScript(null, {file: "alert.js"});
+   }
+   
 });
+
