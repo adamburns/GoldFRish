87d93856c40adc48c21b3becbd2ddd788dfeb865
Adam <me@adamburns.org>
2018-07-11 15:35:26 -0500

    Updated hubspot.js to include a more robust data snatching model using document.querySelector in JavaScript

--- hubspot.js
+++ hubspot.js
@@ -1,20 +1,20 @@
 function setValues(callback){
-	var Name = document.getElementsByClassName("private-expanding-text-input__input")[0].value;
+	var Name = document.querySelector('h2.m-bottom-0 input.private-expanding-text-input__input').value;
 	chrome.storage.local.set({'FR_Name': Name}, function() {
 		console.log('Stored FR_Name as: ' + Name);
 	});
 	
-    var Sponsor = document.getElementsByClassName("m-bottom-1 is--heading-7")[0].innerHTML;
+    var Sponsor = document.querySelector('div.is--module.private-card.private-card__wrapper.private-card--compact.namespaced-hack-section.Section__ScrollWrapper-jajphQ.eSErqF[data-sidebar-key="Company"] div.m-bottom-1.is--heading-7').innerHTML;
     chrome.storage.local.set({'FR_Sponsor': Sponsor}, function() {
         console.log('Stored FR_Sponsor as: ' + Sponsor);
     });
 
-    var ProjectType = document.getElementsByClassName("uiButton private-button private-button--default private-button--form private-form__control private-form__control--inline isInline private-button__dropdown-opener uiDropdown__button private-button--non-link")[2].getElementsByClassName("uiDropdown__buttonContents private-dropdown__button__contents")[0].innerText;
+    var ProjectType = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="dealtype"] span.private-dropdown__button-label.uiDropdown__buttonLabel').innerHTML.replace(/<\!--.*?-->/g, "");
     chrome.storage.local.set({'FR_ProjectType': ProjectType}, function() {
         console.log('Stored FR_ProjectType as: ' + ProjectType);
     });
 
-    var Executed = document.getElementById("uid-ctrl-22").value;
+    var Executed = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="date_engagement_letter_executed"] input.private-expanding-text-input__input.private-datepicker__input[data-field="date_engagement_letter_executed"]').value;
     chrome.storage.local.set({'FR_Executed': Executed}, function() {
         console.log('Stored FR_Executed as: ' + Executed);
     });
