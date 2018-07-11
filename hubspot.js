b4153ec9603dd08b9a8c0337aa9698cafbeb1ec7
Adam <me@adamburns.org>
2018-07-11 10:44:08 -0500

    Converted hubspot.js to callback functions, fixed the result.key error

--- hubspot.js
+++ hubspot.js
@@ -1,29 +1,41 @@
-getValue
+function setValues(callback){
+	var Name = document.getElementsByClassName("private-expanding-text-input__input")[0].value;
+	chrome.storage.local.set({'FR_Name': Name}, function() {
+		console.log('Stored FR_Name as: ' + Name);
+	});
+	
+    var Sponsor = document.getElementsByClassName("m-bottom-1 is--heading-7")[0].innerHTML;
+    chrome.storage.local.set({'FR_Sponsor': Sponsor}, function() {
+        console.log('Stored FR_Sponsor as: ' + Sponsor);
+    });
 
-var Name = document.getElementsByClassName("private-expanding-text-input__input")[0].value;
-chrome.storage.local.set({'FR_Name': Name}, function() {
-//todo
-console.log('FR Name is: ' + Name);
-});
+    var ProjectType = document.getElementsByClassName("uiButton private-button private-button--default private-button--form private-form__control private-form__control--inline isInline private-button__dropdown-opener uiDropdown__button private-button--non-link")[2].getElementsByClassName("uiDropdown__buttonContents private-dropdown__button__contents")[0].innerText;
+    chrome.storage.local.set({'FR_ProjectType': ProjectType}, function() {
+        console.log('Stored FR_ProjectType as: ' + ProjectType);
+    });
 
+    var Executed = document.getElementById("uid-ctrl-17").value;
+    chrome.storage.local.set({'FR_Executed': Executed}, function() {
+        console.log('Stored FR_Executed as: ' + Executed);
+    });
 
-var Sponsor = document.getElementsByClassName("m-bottom-1 is--heading-7")[0].innerHTML;
-chrome.storage.local.set({'FR Sponsor': Sponsor}, function() {
-//todo
-});
-var ProjectType = document.getElementsByClassName("uiButton private-button private-button--default private-button--form private-form__control private-form__control--inline isInline private-button__dropdown-opener uiDropdown__button private-button--non-link")[2].getElementsByClassName("uiDropdown__buttonContents private-dropdown__button__contents")[0].innerText;
-chrome.storage.local.set({'FR ProjectType': ProjectType}, function() {
-//todo
-});
+    var URL = window.location.href;
+    chrome.storage.local.set({'URL': URL}, function() {
+        console.log('Stored URL as: ' + URL); 
+    });
 
-var Executed = document.getElementById("uid-ctrl-17").value;
-chrome.storage.local.set({'FR Executed': Executed}, function() {
-//todo
-});
+	callback(complete);
+}
+function getValues(callback){
+	chrome.storage.local.get(['FR_Name'], function(result) {
+		console.log('Fetched FR_Name as: ' + result.FR_Name);
+	});
+	callback();
+}
 
-var URL = window.location.href;
-chrome.storage.local.set({'FR URL': URL}, function() {
-//todo
-});
+function complete(){
+	alert('The script is complete!');
+}
 
-alert("The HubSpot script has run, click me again on a SharePoint page to copy the data");
+setValues(getValues);
+
