7dd7962692993a5249c020b72f2cf1090b8ae66a
Adam <me@adamburns.org>
2018-09-17 10:01:24 -0500

    Fixed bug where sharepoint.js would not run completely -- fixed by correcting the elementID

--- sharepoint.js
+++ sharepoint.js
@@ -33,15 +33,19 @@ function getValues(callback) {
         }
     });
 
-    callback(complete);
+    setTimeout(callback, 50);
 }
 
 function setSharePointValues(callback) {
-    
-    document.getElementById("FileLeafRef_8553196d-ec8d-4564-9861-3dbe931050c8_$onetidIOFile").value = Name;
-    console.log('Set SharePoint value for FR_Name as: ' + Name);
-    document.getElementById("Sponsor0_$containereditableRegion").innerHTML = Sponsor;
 
+    console.log('Function 2');
+
+    document.getElementById("FileLeafRef_8553196d-ec8d-4564-9861-3dbe931050c8_$onetidIOFile").value = Name;
+    
+    console.log('Sponsor: ' + Sponsor);
+
+    document.getElementById("Sponsor_$containereditableRegion").innerHTML = Sponsor;
+    
     document.getElementById("EL_x0020_Executed_x0020_Date_de12219e-f637-4cdd-a2ca-db8a187fc818_$DateTimeFieldDate").value = Executed;
 
     document.getElementById("LinkToOpportunity_edb7d412-4d8b-4b72-9f81-d22922fb3ba9_$UrlFieldUrl").value = URL;
@@ -52,14 +56,19 @@ function setSharePointValues(callback) {
 
     document.getElementById("ReportStatus1_43c10367-8f65-4d65-867c-6c060116293b_$DropDownChoice").value = "Preliminary";
 
+    document.getElementById("ProjectLead_4c03e8ff-0d1d-4f45-a90b-ddce9abe5cca_$ClientPeoplePicker_InitialHelpText")
+
     document.getElementById("ProjectLead_4c03e8ff-0d1d-4f45-a90b-ddce9abe5cca_$ClientPeoplePicker_InitialHelpText").innerHTML = ProjectLead;
 
     callback();
 }
 
 function complete() {
+    
+    console.log('Function 3');
+    
     alert('The SharePoint script has finished running.');
 }
 
-getValues(setSharePointValues);
+getValues(function(){ setSharePointValues(complete) });
 console.log('Global variable URL: ' + URL);
