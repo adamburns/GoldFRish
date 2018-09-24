415a2c74e3ee61718a76fad770e1a97e2a77202e
Adam <me@adamburns.org>
2018-09-24 16:32:43 -0500

    Added error logging if the HubSpot data scraping fails at some point

--- hubspot.js
+++ hubspot.js
@@ -1,33 +1,75 @@
 function setValues(callback){
-	var Name = document.querySelector('h2.m-bottom-0 input.private-expanding-text-input__input').value;
-	chrome.storage.local.set({'FR_Name': Name}, function() {
-		console.log('Stored FR_Name as: ' + Name);
-	});
-	
-    var Sponsor = document.querySelector('div.is--module.private-card.private-card__wrapper.private-card--compact.namespaced-hack-section.UISection__ScrollWrapper-eQImbY.jpCPEv[data-sidebar-key="Company"] div.m-bottom-1.is--heading-7').innerText;
-    chrome.storage.local.set({'FR_Sponsor': Sponsor}, function() {
-        console.log('Stored FR_Sponsor as: ' + Sponsor);
-    });
+	try
+    {
+        var Name = document.querySelector('h2.m-bottom-0 input.private-expanding-text-input__input').value;
+	    chrome.storage.local.set({'FR_Name': Name}, function() {
+		    console.log('Stored FR_Name as: ' + Name);
+	    });
+	}
+    catch(e)
+    {
+        console.log(e);
+    }
 
-    var ProjectType = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="dealtype"] span.private-dropdown__button-label.uiDropdown__buttonLabel').innerHTML.replace(/<\!--.*?-->/g, "");
-    chrome.storage.local.set({'FR_ProjectType': ProjectType}, function() {
-        console.log('Stored FR_ProjectType as: ' + ProjectType);
-    });
+    try
+    {
+        var Sponsor = document.querySelector('div.is--module.private-card.private-card__wrapper.private-card--compact.namespaced-hack-section.UISection__ScrollWrapper-eQImbY.jpCPEv[data-sidebar-key="Company"] div.m-bottom-1.is--heading-7').innerText;
+        chrome.storage.local.set({'FR_Sponsor': Sponsor}, function() {
+            console.log('Stored FR_Sponsor as: ' + Sponsor);
+        });
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 
-    var Executed = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="date_engagement_letter_executed"] input.private-expanding-text-input__input.private-datepicker__input[data-field="date_engagement_letter_executed"]').value;
-    chrome.storage.local.set({'FR_Executed': Executed}, function() {
-        console.log('Stored FR_Executed as: ' + Executed);
-    });
+    try
+    {
+        var ProjectType = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="dealtype"] span.private-dropdown__button-label.uiDropdown__buttonLabel').innerHTML.replace(/<\!--.*?-->/g, "");
+        chrome.storage.local.set({'FR_ProjectType': ProjectType}, function() {
+            console.log('Stored FR_ProjectType as: ' + ProjectType);
+        });
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 
-    var URL = window.location.href;
-    chrome.storage.local.set({'URL': URL}, function() {
-        console.log('Stored URL as: ' + URL); 
-    });
+    try
+    {
+        var Executed = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="date_engagement_letter_executed"] input.private-expanding-text-input__input.private-datepicker__input[data-field="date_engagement_letter_executed"]').value;
+        chrome.storage.local.set({'FR_Executed': Executed}, function() {
+            console.log('Stored FR_Executed as: ' + Executed);
+        });
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 
-    var ProjectLead = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="project_lead"] span.private-dropdown__button-label.uiDropdown__buttonLabel').innerHTML.replace(/<\!--.*?-->/g, "");
-    chrome.storage.local.set({'ProjectLead': ProjectLead}, function() {
-        console.log('Stored ProjectLead as: ' + ProjectLead);
-    });
+    try
+    {
+        var URL = window.location.href;
+        chrome.storage.local.set({'URL': URL}, function() {
+            console.log('Stored URL as: ' + URL); 
+        });
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+
+    try
+    {
+        var ProjectLead = document.querySelector('div.private-form__set.private-form__set--floating.floating-field-with-border[data-profile-property="project_lead"] span.private-dropdown__button-label.uiDropdown__buttonLabel').innerHTML.replace(/<\!--.*?-->/g, "");
+        chrome.storage.local.set({'ProjectLead': ProjectLead}, function() {
+            console.log('Stored ProjectLead as: ' + ProjectLead);
+        });
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 
 	callback();
 }
