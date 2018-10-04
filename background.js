5cdec8a1cc4f2513e94d7ffdb47e7dfacf73d4d1
Adam <me@adamburns.org>
2018-10-04 14:23:02 -0500

    Added more comments to make things clear

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 chrome.browserAction.onClicked.addListener(function(tabs) {
-   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
    
-      var url = tabs[0].url;
-   
-      if (url.includes("https://factrightllc367.sharepoint.com/_layouts/15/NewDocSet.aspx")) {
-         // if statement
-         chrome.t
-         chrome.tabs.executeScript(null, {file: "sharepoint.js"});
-      } else if (url.includes("https://app.hubspot.com/contacts/")) {
-         // else if statement
-         chrome.t
-         chrome.tabs.executeScript(null, {file: "hubspot.js"});
-      } else {
-         chrome.t
-         chrome.tabs.executeScript(null, {file: "alert.js"});
-      }
-   });
+        var url = tabs[0].url;
+        //if current tab is on SharePoint, run sharepoint.js
+        if (url.includes("https://factrightllc367.sharepoint.com/_layouts/15/NewDocSet.aspx")) {
+            chrome.t
+            chrome.tabs.executeScript(null, {file: "sharepoint.js"});
+        //else if current tab is on HubSpot, run hubspot.js
+        } else if (url.includes("https://app.hubspot.com/contacts/")) {
+            chrome.t
+            chrome.tabs.executeScript(null, {file: "hubspot.js"});
+        } else {
+        //otherwise, if current tab is neither, run alert.js
+            chrome.t
+            chrome.tabs.executeScript(null, {file: "alert.js"});
+        }
+    });
 });
